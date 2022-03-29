ff6e6dad496e2f6a2e2e0fc955b6f5bc42e02717
ashmeetsmonga <ashmeetsmonga@gmail.com>
2022-03-29 23:50:54 +0530

    added 1 project in projects section

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const aboutBtn = document.getElementById("aboutBtn");
+const aboutBtn = document.getElementById("about-btn");
+const projectsBtn = document.getElementById("projects-btn");
 const aboutMeSection = document.getElementById("about-me-section");
+const projectsSection = document.getElementById("projects-section");
 
 aboutBtn.addEventListener("click", () => {
 	aboutMeSection.scrollIntoView({ behavior: "smooth" });
+});
+
+projectsBtn.addEventListener("click", () => {
+	let offSet = 120;
+	let sectionPosition = projectsSection.getBoundingClientRect().top;
+	let offSetPosition = sectionPosition + window.scrollY - offSet;
+
+	window.scrollTo({
+		top: offSetPosition,
+		behavior: "smooth",
+	});
 });
